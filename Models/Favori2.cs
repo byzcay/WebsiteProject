28c12ccb1ce6300e44a89a649cdf557a848f664c
Beyza Çay <116112022+byzcay@users.noreply.github.com>
2023-07-28 10:17:33 +0300

    Add files via upload

---
+++ Models/Favori2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecipeApp.Models;
+
+public partial class Favori2
+{
+    public int Id { get; set; }
+
+    public int? RecipeId { get; set; }
+
+    public string? UserId { get; set; }
+
+    public int? FavValue { get; set; }
+}
---
+++ Models/Ingredient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecipeApp.Models;
+
+public partial class Ingredient
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public string? Unit { get; set; }
+
+    public int? RecipeId { get; set; }
+
+    public int? Calories { get; set; }
+}
