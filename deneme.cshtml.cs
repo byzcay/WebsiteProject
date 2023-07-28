8de25aac59422104b71a2658989749c2f1740d16
Beyza Çay <116112022+byzcay@users.noreply.github.com>
2023-07-28 10:10:26 +0300

    Recipe Website

---
+++ deneme.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecipeApp
+{
+    public class denemeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
